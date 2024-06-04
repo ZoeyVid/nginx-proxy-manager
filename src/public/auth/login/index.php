d82787d01e4137aca09e1ac711c0daea56a7a142
DavidCraftDev <contact@davidcraft.de>
2024-06-04 15:54:22 +0200

    add default/dead host pages by @DavidCraftDev
    Signed-off-by: Zoey <zoey@z0ey.de>
    Co-Authored-By: David <contact@davidcraft.de>

--- src/public/auth/login/index.php
+++ src/public/auth/login/index.php
@@ -21,14 +21,14 @@ if (isAuthenticated()) {
 <html lang="en">
 <head>
     <title>NPMplus - Login</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="application-name" content="NPMplus">
-    <meta name="author" content="ZoeyVid">
-    <meta name="description" content="Login Page for NPMplus">
-    <meta name="keywords" content="NPMplus, login">
-    <link rel="stylesheet" href="/tailwind.css">
-    <link rel="icon" type="image/webp" href="/favicon.webp">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="application-name" content="NPMplus" />
+    <meta name="author" content="ZoeyVid" />
+    <meta name="description" content="Login Page for NPMplus" />
+    <meta name="keywords" content="NPMplus, login" />
+    <link rel="stylesheet" href="/tailwind.css" />
+    <link rel="icon" type="image/webp" href="/favicon.webp" />
     <!--<script src="https://js.hcaptcha.com/1/api.js?hl=en&render=onload&recaptchacompat=off" async defer></script>-->
 </head>
 
@@ -101,5 +101,6 @@ if (isAuthenticated()) {
     ?>
 </div>
 </body>
+</html>
 <?php
 } ?>
